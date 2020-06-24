fabc2eb785e6333d693e5afc65ff7ccde411ffbc
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-06-24 01:08:51 -0300

    Better matching brackets

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,25 +1,17 @@
-const BRACKETS: &str = "()[]{}";
 const OPENING_BRACKETS: &str = "([{";
+const CLOSING_BRACKETS: &str = ")]}";
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let filtered_string: String = string
-        .chars()
-        .filter(|x: &char| BRACKETS.contains(*x))
-        .collect();
-
-    check(filtered_string)
-}
-
-fn check(input: String) -> bool {
     let mut open_brackets: Vec<char> = vec![];
 
-    for char in input.chars().into_iter() {
+    for char in string.chars().into_iter() {
         match char {
             value if OPENING_BRACKETS.contains(value) => open_brackets.push(value),
             '}' if open_brackets.pop() == Some('{') => (),
             ')' if open_brackets.pop() == Some('(') => (),
             ']' if open_brackets.pop() == Some('[') => (),
-            _ => return false,
+            value if CLOSING_BRACKETS.contains(value) => return false,
+            _ => (),
         }
     }
 
